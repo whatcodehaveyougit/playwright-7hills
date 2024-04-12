64c87818341afe989442bbc1a9f6ae6110b7719d
whatcodehaveyougit <sigurdwatt@gmail.com>
2024-04-12 11:04:14 +0100

    Add delay to btn click

--- tests/contact-form.spec.ts
+++ tests/contact-form.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 
 test("Contact Form Test", async ({ page }) => {
   await page.goto("https://7hillsceilidh.com/");
-  await page.getByRole("link", { name: "Contact" }).click();
+  await page.getByRole("link", { name: "Contact" }).click({ delay: 1000 });
   await page.getByPlaceholder("Joe Bloggs").click();
   await page.getByPlaceholder("Joe Bloggs").fill("Sigurd");
   await page.getByPlaceholder("ceilidhmaghee@gmail.com").click();
@@ -18,6 +18,6 @@ test("Contact Form Test", async ({ page }) => {
     )
     .fill("This is a playwright test");
   await expect(page.getByText("Submit")).toBeVisible();
-  await page.getByRole("button", { name: "Submit" }).click();
+  await page.getByRole("button", { name: "Submit" }).click({ delay: 1000 });
   await expect(page.getByText("Thanks for being awesome")).toBeVisible();
 });
